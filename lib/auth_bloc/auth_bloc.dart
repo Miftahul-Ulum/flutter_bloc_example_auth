232a5328cd61a9490919c84c1217b6d5b3697de0
Miftahul-Ulum <aqilajati7@gmail.com>
2024-01-11 22:59:57 +0700

    fixing package

--- lib/auth_bloc/auth_bloc.dart
+++ lib/auth_bloc/auth_bloc.dart
@@ -3,12 +3,15 @@ import 'package:flutter_bloc_example_auth/auth_bloc/auth_event.dart';
 import 'package:flutter_bloc_example_auth/auth_bloc/auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent,AuthState>{
-  AuthBloc() : super(AuthInitial() as AuthState){
-    on<LoginEvent>((Event,emit)){
-emit(AuthLoading());
-Future.delayed(Duration(second:5));
-emit(AuthSuccess(name: 'Ulum'));
-    },);
+  AuthBloc() : super(AuthInitial()) {
+    on<LoginEvent>(
+      // ignore: use_function_type_syntax_for_parameters
+      (event, emit)){
+        emit(AuthLoading());
+        Future.delayed(Duration(seconds: 5));
+        emit(AuthSuccess(name: 'Ulum'));
+        },
+        );
 
     on<LogoutEvent>(
       (event, emit){
@@ -18,7 +21,4 @@ emit(AuthSuccess(name: 'Ulum'));
       },
     );
   }
-}
-
-class AuthInitial {
 }
