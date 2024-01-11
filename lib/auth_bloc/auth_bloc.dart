9fee70bd5ed4c7339904fefdcbe5d82785f62dd0
Miftahul-Ulum <aqilajati7@gmail.com>
2024-01-11 22:49:07 +0700

    auth_bloc fixing

--- lib/auth_bloc/auth_bloc.dart
+++ lib/auth_bloc/auth_bloc.dart
@@ -1,1 +1,24 @@
-class AuthBloc extends
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_example_auth/auth_bloc/auth_event.dart';
+import 'package:flutter_bloc_example_auth/auth_bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent,AuthState>{
+  AuthBloc() : super(AuthInitial() as AuthState){
+    on<LoginEvent>((Event,emit)){
+emit(AuthLoading());
+Future.delayed(Duration(second:5));
+emit(AuthSuccess(name: 'Ulum'));
+    },);
+
+    on<LogoutEvent>(
+      (event, emit){
+        emit(AuthLoading());
+        Future.delayed(const Duration(seconds: 5));
+        emit(AuthInitial());
+      },
+    );
+  }
+}
+
+class AuthInitial {
+}
